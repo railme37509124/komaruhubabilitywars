9409c08514441b330cf0b7bfb6aad35555986107
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-09-21 14:46:04 +0100

    config system like am i goated or am i goated??

--- script.lua
+++ script.lua
@@ -1,11 +1,23 @@
+repeat task.wait() until game:IsLoaded()
+task.wait(1) -- for safety ig
+print("something1")
 -- made by altered (Not discord username)
 -- railme37509124 on github
 -- dumbdog9 on youtube
 -- UI library: KLib V2
 -- Credits to: Infinite Yield, some other script for the ui library inspiration i dont know its name tho lol, and me
-local klib = loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/KLibV2/main/library"))() -- you can go modify this a bit to change the color theme if you would like to
+local liblib = "https://raw.githubusercontent.com/railme37509124/KLibV2/24e69443012d4e5129cf14f0398aebb5d2452db5/library"
+local klib = loadstring(game:HttpGet(liblib))() -- you can go modify this a bit to change the color theme if you would like to
 klib:SetTitle("Komaru Hub | Ability Wars")
+local http = game:GetService("HttpService")
 local backroompos = CFrame.new(-7157, 96, -5071)
+local od = klib.Destroy
+local unloaded = false
+local destroy = function()
+    unloaded = true
+    task.wait(0.01)
+    od()
+end
 local modlist = {
     1756212802,
     4393226871,
@@ -26,15 +38,68 @@ local modlist = {
     533784762,
     110439399,
 }
+
 local plrs = game.Players
+local sets = {
+    WalkSpeedValue = 16,
+    JumpPowerValue = 50,
+    GravityValue = 196.2,
+    AutoArena = false,
+    InfiniteJump = false,
+    ShowPlayers = false,
+    JoinLeaveNotifier = false,
+    AntiModerator = false,
+    AntiVoid = false,
+    LookAtPlayers = false,
+    TargetStrafe = false,
+    KillAura = false,
+    LegitAura = false,
+    AntiGravAbility = false,
+    KillAuraDistanceValue = 0,
+    Reach = false,
+    AvoidTime = false,
+    ReachSlider = 2,
+    GuiDragSpeed = 0.2,
+    AntiVoidColorHex = "#ffffff",
+    AbilityEsp = false,
+    -- KillAura Autofarm will NOT save hahahaha
+}
+local debugging = true
+function writecfg()
+    if not isfile("KOMARUCONFIG.json") then
+        writefile("KOMARUCONFIG.json", http:JSONEncode(sets))
+    end
+end
+
+function savesettings()
+    writefile("KOMARUCONFIG.json", http:JSONEncode(sets))
+    if debugging then 
+        print("--------------------\n" .. http:JSONEncode(sets))
+    end
+end
+
+function loadsettings()
+    writecfg()
+    sets = http:JSONDecode(readfile("KOMARUCONFIG.json"))
+end
+loadsettings()
+if debugging then
+    for i, v in sets do
+        print(i, v)
+    end
+end
+
+
+-- wouldnt be worth the time to change this
 ff = {
     Killaura = false,
     Legitaura = false,
-    Botfarm = false,
+    Botfarm = false, 
     Antigravability = false,
     Lookatplayers = false,
     Targetstrafe
 }
+
 dcl = "https://discord.gg/uSbrxdZ4"
 scr = function(l) return loadstring(game:HttpGet(l))() end
 function lookatplr(plr)
@@ -66,21 +131,34 @@ modconnections = {}
 LocalPlayerTab:Slider({
 	Name = "Walk Speed",
 	Callback = function(value)
+        sets["WalkSpeedValue"] = value
         plrs.LocalPlayer.Character.Humanoid.WalkSpeed = value
 	end,
 	Min = 16,
 	Max = 150,
 	Round = true
-})
+}).Set(sets["WalkSpeedValue"])
 LocalPlayerTab:Slider({
 	Name = "Jump Power",
 	Callback = function(value)
+        sets["JumpPowerValue"] = value
         plrs.LocalPlayer.Character.Humanoid.JumpPower = value
 	end,
 	Min = 50,
 	Max = 250,
 	Round = true
-})
+}).Set(sets["JumpPowerValue"])
+LocalPlayerTab:Slider({
+	Name = "Gravity",
+	Callback = function(value)
+        sets["GravityValue"] = value
+        workspace.Gravity = value
+	end,
+	Min = 0,
+	Max = 196.2,
+	Round = true
+}).Set(sets["GravityValue"])
+
 function hitplr(plr)
     game:GetService("ReplicatedStorage")["Remote Events"].Punch:FireServer(plr.Character)
 end
@@ -100,9 +178,10 @@ function entitynearpositon(dist)
 end
 local avpart = nil
 local istouched
-CombatTab:Toggle({
+local antivoidtoggle = CombatTab:Toggle({
     Name = "Anti Void",
     Callback = function(state)
+        sets["AntiVoid"] = state
         if state then
             avpart = Instance.new("Part")
             avpart.Material = Enum.Material.ForceField
@@ -116,13 +195,15 @@ CombatTab:Toggle({
             avpart.Parent = workspace
             avpart.Touched:Connect(function(prt)
                 if prt.Name == "HumanoidRootPart" and not istouched then
-                    for i = 1, 12 do
-                        plrs.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.X, plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.Y + 26, plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.Z)
-                        task.wait(0.05)
+                    if prt.Parent.Name == plrs.LocalPlayer.Name then
+                        for i = 1, 12 do
+                            plrs.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.X, plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.Y + 26, plrs.LocalPlayer.Character.HumanoidRootPart.Velocity.Z)
+                            task.wait(0.05)
+                        end
+                        istouched = true
+                        task.wait(.5)
+                        istouched = false
                     end
-                    istouched = true
-                    task.wait(.5)
-                    istouched = false
                 end
             end)
         else
@@ -132,6 +213,7 @@ CombatTab:Toggle({
         end
     end
 })
+if sets["AntiVoid"] then antivoidtoggle.Click() end
 CombatTab:TextBox({
 	Name = "AntiVoid Color",
 	ClearOnFocus = true,
@@ -148,53 +230,69 @@ CombatTab:TextBox({
         end
 	end,
 })
-CombatTab:Toggle({
+local lookatplayerstoggle = CombatTab:Toggle({
     Name = "LookAt Players",
     Callback = function(state)
+        sets["LookAtPlayers"] = state
         ff.Lookatplayers = state
     end
 })
-CombatTab:Toggle({
+if sets["LookAtPlayers"] then lookatplayerstoggle.Click() end
+
+local targetstrafetoggle = CombatTab:Toggle({
 	Name = "TargetStrafe",
 	Callback = function(state)
+        sets["TargetStrafe"] = state
         ff.Targetstrafe = state
 	end
 })
+if sets["TargetStrafe"] then targetstrafetoggle.Click() end
+
 local karange = 10
-CombatTab:Toggle({
+local katoggle = CombatTab:Toggle({
     Name = "KillAura",
     Callback = function(state)
-         ff.Killaura = state
-         print("tg: ".. tostring(state))
+        sets["KillAura"] = state
+        ff.Killaura = state
+        if debugging then print("tg: ".. tostring(state)) end
     end
 })
-CombatTab:Toggle({
+if sets["katoggle"] then katoggle.Click() end
+
+local latoggle = CombatTab:Toggle({
     Name = "LegitAura",
     Callback = function(state)
+        sets["LegitAura"] = state
         ff.Legitaura = state
         print("tg: ".. tostring(state))
     end
 })
-CombatTab:Toggle({
+if sets["LegitAura"] then latoggle.Click() end
+
+local antgtoggle = CombatTab:Toggle({
     Name = "Anti Gravity Ability",
     Callback = function(state)
+        sets["AntiGravAbility"] = state
         ff.Antigravability = state
         print("tg: ".. tostring(state))
     end
 })
-CombatTab:Slider({
+if sets["AntiGravAbility"] then antgtoggle.Click() end
+local kadistance = CombatTab:Slider({
 	Name = "KillAura Distance threshold",
 	Callback = function(value)
+        sets["KillAuraDistanceValue"] = value
         karange = value
 	end,
 	Min = 10,
 	Max = 50,
 	Round = true
-})
+}).Set(sets["KillAuraDistanceValue"])
 local lcharadded2 = nil
-LocalPlayerTab:Toggle({
+local autoarenatog = LocalPlayerTab:Toggle({
 	Name = "Auto Arena",
 	Callback = function(state)
+        sets["AutoArena"] = state
         if state then
             lcharadded2 = plrs.LocalPlayer.CharacterAdded:Connect(function()
                 task.wait(plrs.LocalPlayer:GetNetworkPing())
@@ -216,6 +314,7 @@ LocalPlayerTab:Toggle({
         end
 	end
 })
+if sets["AutoArena"] then autoarenatog.Click() end
 local Highlights = {}
 local Charadded = {}
 local Plradded = nil
@@ -223,9 +322,10 @@ local function addhighlight(plr)
     local hl = Instance.new("Highlight", plr.Character)
     Highlights[plr] = hl
 end
-LocalPlayerTab2:Toggle({
+local showplayerstog = LocalPlayerTab2:Toggle({
 	Name = "Show players",
 	Callback = function(state)
+        sets["ShowPlayers"] = state
         if state then
             for _, v in plrs:GetPlayers() do
                 if v.Character ~= nil then
@@ -256,6 +356,8 @@ LocalPlayerTab2:Toggle({
         end
 	end
 })
+if sets["ShowPlayers"] then showplayerstog.Click() end
+
 local SelectionBoxes = {}
 local Charadded2 = {}
 local Plradded2 = nil
@@ -264,9 +366,10 @@ local function addbox(plr)
     sb.Adornee = plr.Character
     SelectionBoxes[plr] = sb
 end
-LocalPlayerTab2:Toggle({
+local showboxestog = LocalPlayerTab2:Toggle({
 	Name = "Player Boxes",
 	Callback = function(state)
+        sets["ShowBoxes"] = state
         if state then
             for _, v in plrs:GetPlayers() do
                 if v.Character ~= nil then
@@ -297,6 +400,7 @@ LocalPlayerTab2:Toggle({
         end
 	end
 })
+if sets["ShowBoxes"] then showboxestog.Click() end
 local BBGuis = {}
 local Charadded4 = {}
 local Plradded4 = nil
@@ -305,16 +409,18 @@ local function addbb(plr)
     h.Parent = plr.Character.Head
     h.Name = "abilesp"
     h.TextLabel.Text = plr.leaderstats.Ability.Value
-    h.StudsOffset = Vector3.new(0, -0.5, 0)
+    h.StudsOffset = Vector3.new(0, -3, 0)
+    h.Size = UDim2.new(0, 200, 0, 200)
     h.AlwaysOnTop = true
     plr.leaderstats.Ability:GetPropertyChangedSignal("Value"):Connect(function()
         h.TextLabel.Text = plr.leaderstats.Ability.Value
     end)
     BBGuis[plr] = h
 end
-LocalPlayerTab2:Toggle({
+abilesptog = LocalPlayerTab2:Toggle({
 	Name = "Ability ESP",
 	Callback = function(state)
+        sets["AbilityEsp"] = state
         if state then
             for _, v in plrs:GetPlayers() do
                 if v.Character ~= nil then
@@ -348,6 +454,7 @@ LocalPlayerTab2:Toggle({
         end
 	end
 })
+if sets["AbilityEsp"] then abilesptog.Click() end
 
 plraddedj = nil
 plraddedl = nil
@@ -399,7 +506,7 @@ LocalPlayerTab2:Toggle({
     Default = true
 })
 local infjumpcon = nil
-LocalPlayerTab:Toggle({
+local infjumptog = LocalPlayerTab:Toggle({
 	Name = "Infinite Jump",
 	Callback = function(state)
         if state then
@@ -413,11 +520,35 @@ LocalPlayerTab:Toggle({
         end
 	end
 })
+if sets["InfiniteJump"] then infjumptog.Click() end
+LocalPlayerTab:TextBox({
+	Name = "Fake Punches",
+	ClearOnFocus = false,
+	ClearOnLost = false,
+	EnterPressed = false,
+	Placeholder = "Fake Punches",
+	Callback = function(text)
+        if tonumber(text) ~= nil then -- prevent letters because people are stupid
+            game:GetService("Players").LocalPlayer.PlayerGui.Counter.Frame.TextLabel.Text = tonumber(text)
+        end
+	end,
+})
 local lcharadded = nil
-local reachval = 10
-CombatTab:Toggle({
+local reachval = 2
+CombatTab:Slider({
+	Name = "Reach Range",
+	Callback = function(value)
+        sets["ReachSlider"] = value
+        reachval = value
+	end,
+	Min = 2,
+	Max = 30,
+	Round = true
+}).Set(sets["ReachSlider"])
+local reachtog = CombatTab:Toggle({
 	Name = "Reach",
 	Callback = function(state)
+        sets["Reach"] = state
         if state then
             plrs.LocalPlayer.Character.Hitbox.Size = Vector3.new(reachval, reachval, reachval)
             lcharadded = plrs.LocalPlayer.CharacterAdded:Connect(function()
@@ -431,25 +562,41 @@ CombatTab:Toggle({
         end
 	end
 })
-CombatTab:Slider({
-	Name = "Reach Range",
-	Callback = function(value)
-        reachval = value
-	end,
-	Min = 10,
-	Max = 30,
-	Round = true
+if sets["Reach"] then reachtog.Click() end
+cadded = nil
+local avtime = CombatTab:Toggle({
+	Name = "Avoid Time",
+	Callback = function(state)
+        sets["AvoidTime"] = state
+        if state then
+            cadded = workspace.ChildAdded:Connect(function(v)
+                if v.Name == "1" then
+                    v.CanCollide = true
+                    if (plrs.LocalPlayer.Character.HumanoidRootPart.Position - v.Position).magnitude < 112 then
+                        plrs.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame + Vector3.new(0, 99, 0)
+                        plrs.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+                    end
+                end
+            end)
+        else
+            if cadded then
+                cadded:Disconnect()
+            end
+        end
+	end
 })
+if sets["AvoidTime"] then avtime.Click() end
 SettingsTab:Slider({
 	Name = "Gui Drag Speed",
 	Callback = function(value)
+        sets["GuiDragSpeed"] = value
         klib.DragSpeed = value
 	end,
 	Min = 0,
 	Max = 1,
     Default = klib.DragSpeed,
 	Round = false
-})
+}).Set(sets["GuiDragSpeed"])
 local selected = nil
 local cti = tick()
 CombatTab:Toggle({
@@ -477,18 +624,20 @@ SettingsTab:Button({
         setclipboard(dcl)
     end
 })
-local zzz = {}
-for i, v in workspace["Start Abilities"]:GetChildren() do
-    table.insert(zzz, v.Name)
-end
-table.sort(zzz)
-CombatTab2:DropDown({
-    Name = "Equip Ability",
-    List = zzz,
-    Callback = function(a)
-        game:GetService("ReplicatedStorage")["Remote Events"].AbilitySelect:FireServer(a)
+if game.PlaceId == 8260276694 then
+    local zzz = {}
+    for i, v in workspace["Start Abilities"]:GetChildren() do
+        table.insert(zzz, v.Name)
     end
-})
+    table.sort(zzz)
+    CombatTab2:DropDown({
+        Name = "Equip Ability",
+        List = zzz,
+        Callback = function(a)
+            game:GetService("ReplicatedStorage")["Remote Events"].AbilitySelect:FireServer(a)
+        end
+    })
+end
 
 task.spawn(function()
     repeat
@@ -585,6 +734,13 @@ task.spawn(function()
         end
         task.wait(0.1)
     until nil
+end)
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/railme37509124/komaruhubabilitywars/refs/heads/main/script.lua'))()")
+end)
+print("something2")
+task.spawn(function()
+    repeat task.wait(5) savesettings() until unloaded
 end)
 game.StarterGui:SetCore("SendNotification", {
     Title = "Loaded",
